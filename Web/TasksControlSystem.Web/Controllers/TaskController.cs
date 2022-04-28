6fd223ba487367dea188ff09f28db285e6102297
SlaviSolakchiev <78353852+SlaviSolakchiev@users.noreply.github.com>
2022-04-28 15:19:55 +0300

    ModifyTaskController

--- Web/TasksControlSystem.Web/Controllers/TaskController.cs
+++ Web/TasksControlSystem.Web/Controllers/TaskController.cs
@@ -1,13 +1,48 @@
 ﻿namespace TasksControlSystem.Web.Controllers
 {
+    using System.Linq;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using TasksControlSystem.Data.Common.Repositories;
+    using TasksControlSystem.Data.Models;
+    using TasksControlSystem.Services.Data;
+    using TasksControlSystem.Web.ViewModels;
 
     public class TaskController : BaseController
     {
+        private readonly IGetCategoriesService getCategoriesService;
+        private readonly IGetLocationsService getLocationsService;
+        private readonly ICreateTaskService createTaskService;
+
+        public TaskController(IGetCategoriesService getCategoriesService, IGetLocationsService getLocationsService, ICreateTaskService createTaskService)
+        {
+            this.getCategoriesService = getCategoriesService;
+            this.getLocationsService = getLocationsService;
+            this.createTaskService = createTaskService;
+        }
+
+        [HttpGet]
         public IActionResult CreateTask()
         {
+            var categoriesItems = this.getCategoriesService.GetCategories();
+            var locationItems = this.getLocationsService.GetLocations();
+
+            var viewModel = new CreateTaskInputModel();
+            viewModel.Locations = locationItems;
+            viewModel.Categories = categoriesItems;
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTask(CreateTaskInputModel input)
+        {
+            if (this.ModelState.IsValid)
+            {
+                this.createTaskService.CreateTask(input);
+            }
             return this.View();
         }
     }
